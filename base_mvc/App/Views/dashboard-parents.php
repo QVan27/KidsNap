7e0c046c98b0d0c38093b4691017f98b9695cad2
safia medjahed <safia.medjahed@gmail.com>
2021-03-24 11:10:20 +0100

    dashboard-parents

--- base_mvc/App/Views/dashboard-parents.php
+++ base_mvc/App/Views/dashboard-parents.php
@@ -6,5 +6,18 @@
             <a href="index.php?page=map" class="btn color-2 btnParents1">une place en crèche</a>
             <a href="index.php?page=map" class="btn color-2 btnParents2">une assistante marternelle</a>
         </div>
+        <h3 class="dash-text">Profil</h3>
+    </div>
+    <div class=" sheet-dash">
+        <div class="pp">
+            <img class="pp-size" src="asset/image/pp4.svg" alt="photo profil">
+            <p class="ppmail">jeannemarie@gmail.com</p>
+            <a href="index.php?page=profil" class="btnprofil">Modifier profil</a>
+        </div>
+        <div class="sheet-wrap">
+            <div id="sheet-two" class="dash-sheet">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br><br> dolores debitis sunt harum velit iste eius explicabo. Eum aut veniam corporis fugiat? <br><br>Repudiandae accusamus ipsam amet accusantium aspernatur enim modi pariatur, optio necessitatibus beatae atque, <br><br>nihil non asperiores velit! Nam non quisquam blanditiis deserunt accusantium cum similique quas quae quam?</p>
+            </div>
+        </div>
     </div>
 </section>
