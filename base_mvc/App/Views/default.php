178b03e48444eb5ac016e4828c33025fb2307f55
JulienMorel76350 <julien.morel76350@gmail.com>
2021-03-24 10:56:12 +0100

    map design

--- base_mvc/App/Views/default.php
+++ base_mvc/App/Views/default.php
@@ -1,19 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kids'Nap</title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Kids'Nap</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+        <link rel="stylesheet" href="asset/css/style.css">
 
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
-    <link rel="stylesheet" href="asset/css/style.css">
-
-</head>
+    </head>
 
 
 <body>
@@ -202,13 +201,11 @@
 
             </div>
 
-        </div>
-
-    </footer>
-    <script src="../../public/asset/js/map.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap&libraries=&v=weekly" async></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="asset/js/main.js"></script>
-</body>
+        </footer>
+        <script src="asset/js/map.js"></script>
+        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+        <script src="asset/js/main.js"></script>
+    </body>
 
 </html>
--- base_mvc/public/asset/js/map.js
+++ base_mvc/public/asset/js/map.js
@@ -1,3 +1,160 @@
-function initMap(){
-    
+// Variables globales
+let ville = distance = ""
+
+window.onload = () => {
+    // On intialise la carte
+    let carte = L.map('map').setView([48.852969, 2.349903], 13)
+
+    // On charge les "tuiles"
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+        // Il est toujours bien de laisser le lien vers la source des données
+        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+        minZoom: 1,
+        maxZoom: 20,
+        name: 'tiles' // Permettra de ne pas supprimer cette couche
+    }).addTo(carte)
+
+    // Gestion des champs
+    let champVille = document.getElementById('champ-ville')
+    let champDistance = document.getElementById('champ-distance')
+    let valeurDistance = document.getElementById('valeur-distance')
+    let adresse = document.getElementById('adresse')
+    let name = document.getElementById('name')
+    let select = document.getElementById('select')
+    var combo = document.createElement('select');
+    $('#form_map').submit(function(e){
+        e.preventDefault();
+    })
+
+
+    champVille.addEventListener("change", function () {
+        // On envoie le requête ajax vers Nominatim
+        ajaxGet(`https://nominatim.openstreetmap.org/search?q=${this.value}&format=json&addressdetails=1&limit=1&polygon_svg=1`)
+            .then(reponse => {
+                // On convertit la réponse en objet Javascript
+                let data = JSON.parse(reponse)
+
+                // On stocke les coordonnées dans ville
+                ville = [data[0].lat, data[0].lon]
+
+                // On centre la carte sur la ville
+                carte.panTo(ville)
+            })
+    })
+    adresse.addEventListener("change", function (e) {
+        ajaxGet(`https://nominatim.openstreetmap.org/search?q=${this.value}&format=json`)
+            .then(reponse => {
+                // On convertit la réponse en objet Javascript
+                let data = JSON.parse(reponse)
+                console.log(data)
+                // Icon options
+                var iconOptions = {
+                    iconUrl: 'asset/image/marker_creche.png',
+                    iconSize: [45, 50]
+                }
+
+                // Creating a custom icon
+                var customIcon = L.icon(iconOptions);
+                var markerOptions = {
+                    icon: customIcon
+                }
+                $('#name').css('display', 'flex');
+                if (data.length > 1) {
+                    // combo.id = 'name';
+                    // select.appendChild(combo);
+                    for (let index = 0; index < data.length; index++) {
+                        $('#name').append('<option value="' + data[index].display_name + '" id="test">' + data[index].display_name + '</option>');
+
+                    }
+
+                } else {
+                    $('#name').innerHTML = "";
+                    ville = [data[0].lat, data[0].lon]
+
+                    let marker = L.marker([data[0].lat, data[0].lon], markerOptions).addTo(carte)
+                    marker.bindPopup(data[0].display_name)
+
+                }
+                // On stocke les coordonnées dans ville
+
+
+                // On centre la carte sur la ville
+                carte.panTo(ville)
+            })
+    })
+    name.addEventListener("change", function (e) {
+        let selectedItem = $(this).children("option:selected").val();
+        var howManyCommasDoIHave = selectedItem.replace(/,/g, '');
+        adresse.value = howManyCommasDoIHave
+    })
+
+
+    champDistance.addEventListener("change", function () {
+        distance = this.value
+
+        valeurDistance.innerText = distance + " km"
+
+        // On vérifie si on a une ville
+        if (ville != "") {
+            // On envoie la requête
+            ajaxGet(`/mvc/Kidsnap/KidsNap/base_mvc//App/Views/map/chargeAgences.php/.php?lat=${ville[0]}&lon=${ville[1]}&distance=${distance}`)
+                .then(reponse => {
+                    // On supprime toutes les couches de la carte
+                    carte.eachLayer(function (layer) {
+                        if (layer.options.name != 'tiles') carte.removeLayer(layer)
+                    })
+
+                    //On trace un cercle correspondant à la distance souhaitée
+                    let circle = L.circle(ville, {
+                        color: "#839c49",
+                        fillColor: "#839c49",
+                        fillOpacity: 0.3,
+                        radius: distance * 1000
+                    }).addTo(carte)
+
+                    // On boucle sur les données
+                    console.log(reponse);
+                    let donnees = JSON.parse(reponse)
+
+                    Object.entries(donnees).forEach(agence => {
+                        // On crée le marqueur
+                        let marker = L.marker([agence[1].lat, agence[1].lon]).addTo(carte)
+                        marker.bindPopup(agence[1].nom)
+
+                    })
+                    // On centre la carte sur le cercle
+                    let bounds = circle.getBounds()
+                    carte.fitBounds(bounds)
+                })
+        }
+    })
+}
+
+/**
+ * Cette fonction effectue un appel Ajax vers une url et retourne une promesse
+ * @param {string} url 
+ */
+function ajaxGet(url) {
+    return new Promise(function (resolve, reject) {
+        // Nous allons gérer la promesse
+        let xmlhttp = new XMLHttpRequest()
+
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4) {
+                if (xmlhttp.status == 200) {
+                    // On "résoud" la promesse
+                    resolve(xmlhttp.response)
+                } else {
+                    reject(xmlhttp)
+                }
+            }
+        }
+
+        xmlhttp.onerror = function (error) {
+            reject(error)
+        }
+
+        xmlhttp.open('get', url, true)
+        xmlhttp.send()
+    })
 }
