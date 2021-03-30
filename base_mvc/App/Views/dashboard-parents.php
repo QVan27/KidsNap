49a6a8faacd12af75fbfbf9a37f57e1c236c8993
JulienMorel76350 <julien.morel76350@gmail.com>
2021-03-30 10:35:55 +0200

    add securite dashboard

--- base_mvc/App/Views/dashboard-parents.php
+++ base_mvc/App/Views/dashboard-parents.php
@@ -1,24 +1,32 @@
-<section class="dashboardParents">
-    <div class="headParents">
-        <img src="asset/image/vector_dash.png" alt="">
-        <h2>Bonjour, de quoi avez-vous besoin aujourd'hui ?</h2>
-        <div class="btnParents">
-            <a href="index.php?page=map" class="btn color-2 btnParents1">une place en crèche</a>
-            <a href="index.php?page=map" class="btn color-2 btnParents2">une assistante marternelle</a>
-        </div>
-        <h3 class="dash-text">Profil</h3>
-    </div>
-    <div class=" sheet-dash">
-        <div class="pp">
-            <img class="pp-size" src="asset/image/pp4.svg" alt="photo profil">
-            <p class="ppmail">Jeanne Marie - 24ans</p>
-            <p class="ppmail">jeannemarie@gmail.com</p>
-            <a href="index.php?page=profil-parents" class="btnprofil">Modifier profil</a>
-        </div>
-        <div class="sheet-wrap">
-            <div id="sheet-two" class="dash-sheet">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br><br> dolores debitis sunt harum velit iste eius explicabo. Eum aut veniam corporis fugiat? <br><br>Repudiandae accusamus ipsam amet accusantium aspernatur enim modi pariatur, optio necessitatibus beatae atque, <br><br>nihil non asperiores velit! Nam non quisquam blanditiis deserunt accusantium cum similique quas quae quam?</p>
+<?php if (isset($_SESSION["user"])) : ?>
+    <?php if ($_SESSION["user"]->user_parent == 1) : ?>
+        <section class="dashboardParents">
+            <div class="headParents">
+                <img src="asset/image/vector_dash.png" alt="">
+                <h2>Bonjour, de quoi avez-vous besoin aujourd'hui ?</h2>
+                <div class="btnParents">
+                    <a href="index.php?page=map" class="btn color-2 btnParents1">une place en crèche</a>
+                    <a href="index.php?page=map" class="btn color-2 btnParents2">une assistante marternelle</a>
+                </div>
+                <h3 class="dash-text">Profil</h3>
             </div>
-        </div>
-    </div>
-</section>
+            <div class=" sheet-dash">
+                <div class="pp">
+                    <img class="pp-size" src="asset/image/pp4.svg" alt="photo profil">
+                    <p class="ppmail">Jeanne Marie - 24ans</p>
+                    <p class="ppmail">jeannemarie@gmail.com</p>
+                    <a href="index.php?page=profil-parents" class="btnprofil">Modifier profil</a>
+                </div>
+                <div class="sheet-wrap">
+                    <div id="sheet-two" class="dash-sheet">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br><br> dolores debitis sunt harum velit iste eius explicabo. Eum aut veniam corporis fugiat? <br><br>Repudiandae accusamus ipsam amet accusantium aspernatur enim modi pariatur, optio necessitatibus beatae atque, <br><br>nihil non asperiores velit! Nam non quisquam blanditiis deserunt accusantium cum similique quas quae quam?</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php else: ?>
+        <?php header('Location: index.php?page=home') ?>
+
+
+    <?php endif; ?>
+<?php endif; ?>
