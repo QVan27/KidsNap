4befb669b0defe8ca4f0fc8553c2ee375623cf04
safia medjahed <safia.medjahed@gmail.com>
2021-03-31 17:31:55 +0200

    disponibilite

--- base_mvc/App/Views/dashboard-pros.php
+++ base_mvc/App/Views/dashboard-pros.php
@@ -9,21 +9,6 @@
                     <a href="index.php?page=disponibilite" class="btn color-2 btnPros"><i class="fas fa-calendar-alt" style="color: var(--white);"></i> Disponibilité</a>
                 </div>
                 <h3 class="dash-text">Profil</h3>
-    </div>
-    <div class=" sheet-dash">
-        <div class="pp">
-            <img class="pp-size" src="asset/image/pp12.svg" alt="photo profil">
-            <p class="ppmail">Pierre Kiroule</p>
-            <p class="ppmail">pierrekiroule@gmail.com</p>
-            <p class="ppmail">Crèche</p>
-            <a href="index.php?page=profil-pros" class="btnprofil btnprofil1">Modifier profil</a>
-        </div>
-        <div class="sheet-wrap">
-            <div id="sheet-two" class="dash-sheet">
-                <p>Notre micro-crèche est située en plein cœur de Rouen derrière les Galeries Lafayette. Celle-ci accueille à la journée pas plus de 10 enfants d'âges mélangés, de 10 semaines à 4 ans révolue, de 8 heure à 18 heure 30.<br><br>
-                    La pédagogie de la micro-crèche est basée sur l'autonomie de l'enfant. Que ce soit par la motricité libre, par la manière dont nous donnons les repas ou par notre volonté de laisser l'enfant faire seul, l'équipe éducative mettra tout en oeuvre pour que votre enfant puisse s'épanouir et devenir autonome.<br><br>
-                    L'équipe éducative, qui est constituée d'un référent technique éducatrice de jeunes enfants et de deux animatrices petite enfance, veille à vous accueillir, vous et votre enfant dans la bienveillance et la confiance. La micro-crèche est un lieu ouvert aux parents, où il est possible pour eux de se poser avec leur enfant et de partager un temps de jeux pendant les transmissions du matin ou du soir.<br><br>
-                    Grâce à sa collaboration avec un site de gestion de crèche, notre équipe mettra tout en oeuvre pour vous rassurer durant la journée, via des photos par exemple.</p>
             </div>
             <div class=" sheet-dash">
                 <div class="pp">
@@ -32,7 +17,6 @@
                     <p class="ppmail">pierrekiroule@gmail.com</p>
                     <p class="ppmail">Crèche</p>
                     <a href="index.php?page=profil-pros" class="btnprofil btnprofil1">Modifier profil</a>
-                    <a href="index.php?page=reservation" class="btnprofil btnprofil1">Réserver un créneau</a>
                 </div>
                 <div class="sheet-wrap">
                     <div id="sheet-two" class="dash-sheet">
@@ -48,6 +32,7 @@
                             <p class="ppmail">pierrekiroule@gmail.com</p>
                             <p class="ppmail">Crèche</p>
                             <a href="index.php?page=profil-pros" class="btnprofil btnprofil1">Modifier profil</a>
+                            <a href="index.php?page=reservation" class="btnprofil btnprofil1">Réserver un créneau</a>
                         </div>
                         <div class="sheet-wrap">
                             <div id="sheet-two" class="dash-sheet">
@@ -56,6 +41,7 @@
                                     L'équipe éducative, qui est constituée d'un référent technique éducatrice de jeunes enfants et de deux animatrices petite enfance, veille à vous accueillir, vous et votre enfant dans la bienveillance et la confiance. La micro-crèche est un lieu ouvert aux parents, où il est possible pour eux de se poser avec leur enfant et de partager un temps de jeux pendant les transmissions du matin ou du soir.<br><br>
                                     Grâce à sa collaboration avec un site de gestion de crèche, notre équipe mettra tout en oeuvre pour vous rassurer durant la journée, via des photos par exemple.</p>
                             </div>
+
                         </div>
                     </div>
         </section>
